6d203b985c27e259c1527ee1c6c56445549ee2fd
emmahoggett <emma.hoggett@epfl.ch>
2021-01-16 13:25:11 +0100

    shrink

--- source/P_Controls.c
+++ source/P_Controls.c
@@ -10,7 +10,7 @@
 #include "P_GamePlay.h"
 
 
-int jump = 0, iter = 50;
+int jump = 0, iter = 30;
 
 
 void handleKeys(){
@@ -21,7 +21,7 @@ void handleKeys(){
     	//Modify position of the sprite accordingly
     	if((keys & KEY_RIGHT)) Gameplay_handleInput(RIGHT);
     	if((keys & KEY_DOWN)) Gameplay_handleInput(DOWN);
-    	if((keysDown() & KEY_UP)) Gameplay_handleInput(UP);
+    	if((keys & KEY_UP)) Gameplay_handleInput(UP);
     	if((keys & KEY_LEFT)) Gameplay_handleInput(LEFT);
 
     	//Make the car jump
@@ -35,8 +35,7 @@ void handleTouch(){
 	 * Condition to make the car jump - the double touch
 	 * 		- Screen not touched : jump = 1
 	 * 		- Screen touched & jump == 1 : jump = 2
-	 * 		- Screen not touched & jump == 2 : jump = 3
-	 * 		- Screen touched & jump == 3 : Make the jump action
+	 * 		- Screen not touched & jump == 2 : Make the jump action
 	 * There is a counter for the third condition, such that the jump is not
 	 * performed automatically.
 	 *
@@ -48,27 +47,27 @@ void handleTouch(){
 	//Read the touched position
 	touchRead(&touch);
 	x = touch.px; y = touch.py;
-	if (x || y){ //The screen is touched
+	if (keysHeld() & KEY_TOUCH){ //The screen is touched
 		//Modify position of the sprite accordingly
-		if (jump != 3){
-			if (x < Get_Car_Pos())  Gameplay_handleInput(LEFT);
-			else if (x > Get_Car_Pos()) Gameplay_handleInput(RIGHT);
-			if (y < 30)  Gameplay_handleInput(UP);
-			else if (y > 162) Gameplay_handleInput(DOWN);
-		}
-
-		if (jump == 1) jump = 2; //Screen touched & jump == 1 : jump = 2
-		else if (jump == 3) { // Screen touched & jump == 3 : Make the jump action
-			jump = 0;Gameplay_handleInput(JUMP);
-		}
-		iter--; // counter
-		if (iter == 0){
-			iter = 50; jump = 0;
-		}
-	} else if (x == 0 && y == 0){ // The screen is not touched
-		if (jump == 0)jump = 1; // Screen not touched : jump = 1
-		else if (jump == 2) jump = 3; //Screen not touched & jump == 2 : jump = 3
+		if (x < Get_Car_Pos())  Gameplay_handleInput(LEFT);
+		else if (x > Get_Car_Pos()) Gameplay_handleInput(RIGHT);
+		if (y < 30)  Gameplay_handleInput(UP);
+		else if (y > 162) Gameplay_handleInput(DOWN);
 	}
+	// Handling the jump action
+	if (x==0 && y==0){
+		// The touch screen is untouched
+		if (jump == 0) jump = 1;
+		else if (jump == 2) {
+			Gameplay_handleInput(JUMP);
+			jump = 0;
+		} // The touch screen is touched
+	} else  if ((keysHeld() & KEY_TOUCH) && (jump == 1)) jump = 2;
+	iter--;
+	if (iter == 0) {
+		jump = 0; iter = 30;
+	}
+
 }
 
 
